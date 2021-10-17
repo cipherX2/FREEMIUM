ac908b864b58be6dcd88b3ea65fd0a96fce0132a
Dinesh <dineshpattnaik23@gmail.com>
2021-10-17 16:31:41 +0530

    Enabled bcrypt algorithm :)

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require("mongoose")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
+ 
 
 
 const app = express();
@@ -57,34 +59,37 @@ app.get("/about", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(function (err) {
-        if (!err) {
-            res.render("success")
-        } else {
-            console.log(err)
-        }
+    bcrypt.hash(req.body.password,saltRounds,function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function (err) {
+            if (!err) {
+                res.render("success")
+            } else {
+                console.log(err)
+            }
+        });
     });
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username
-    const password = md5(req.body.password)
+    const username = req.body.username;
+    const password = req.body.password;
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("success")
-                } else {
-                    res.send("Incorrect Password")
-                };
+                bcrypt.compare(password, foundUser.password, function (err,results) {
+                    if(results == true){
+                        res.render("success");
+                    };
+                });
             };
         };
     });
